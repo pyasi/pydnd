ed8f439c79b9604e1f319f0e2da2b7c006e7fdca
pyasi <pyasi8192@gmail.com>
2018-04-02 23:28:58 -0400

    Allow getting full list of new models

--- pydnd/skills/urls.py
+++ pydnd/skills/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import SpecialAbilityList, SpecialAbilityGet, ActionList, ActionGet
+from .views import SpecialAbilityList, SpecialAbilityGet, ActionList, ActionGet, ReactionList, LegendaryActionList
 
 urlpatterns = [
     path("special_abilities", SpecialAbilityList.as_view(), name="special_ability"),
     path("special_abilities/<str:name_or_id>", SpecialAbilityGet.as_view(), name="special_ability"),
     path("actions", ActionList.as_view(), name="actions"),
-    path("actions/<str:name_or_id>", ActionGet.as_view(), name="actions")
+    path("actions/<str:name_or_id>", ActionGet.as_view(), name="actions"),
+    path("reactions", ReactionList.as_view(), name="reactions"),
+    path("legendary_actions", LegendaryActionList.as_view(), name="legendary_actions"),
 ]
