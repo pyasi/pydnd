2acbfecdff7c4c4d92b6a28214983ef5dc1177a0
zbeermann <31114701+zbeermann@users.noreply.github.com>
2018-04-03 21:04:12 -0400

    s's

--- pydnd/mechanics/urls.py
+++ pydnd/mechanics/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import LanguageList, ConditionList, DamageTypeList
 
 urlpatterns = [
-    path("language/", LanguageList.as_view(), name="language"),
-    path("condition/", ConditionList.as_view(), name="condition"),
-    path("damagetype/", DamageTypeList.as_view(), name="damagetype"),
+    path("languages/", LanguageList.as_view(), name="languages"),
+    path("conditions/", ConditionList.as_view(), name="conditions"),
+    path("damagetypes/", DamageTypeList.as_view(), name="damagetypes"),
 ]
