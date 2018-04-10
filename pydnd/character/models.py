225929377d103f17894136d69aaa8e6fe76bc6eb
pyasi <pyasi8192@gmail.com>
2018-04-09 20:01:57 -0400

    Models and serializers

--- pydnd/character/models.py
+++ pydnd/character/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class AbilityScore(models.Model):
+
+    name = models.CharField(max_length=100, unique=True)
+    full_name = models.CharField(max_length=100)
+    desc = models.CharField(max_length=10000)
+
+    def __str__(self):
+        return self.full_name
+
+
+class Skill(models.Model):
+
+    name = models.CharField(max_length=100, unique=True)
+    desc = models.CharField(max_length=10000)
+    ability_score = models.ForeignKey(AbilityScore, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
--- pydnd/character/views.py
+++ pydnd/character/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework import generics, status
 
-# Create your views here.
+from .models import AbilityScore
+from .serializers import AbilityScoreListSerializer, AbilityScoreSerializer
+
+
+#List of all equipment categories
+class AbilityScoreList(generics.ListCreateAPIView):
+
+    queryset = AbilityScore.objects.all()
+    serializer_class = AbilityScoreListSerializer
