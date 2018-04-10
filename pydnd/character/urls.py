eddfd2eb34e5fe8bb36559956fba1ac4f5573676
pyasi <pyasi8192@gmail.com>
2018-04-09 22:11:16 -0400

    Finish up

--- pydnd/character/urls.py
+++ pydnd/character/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import AbilityScoreList, SkillList
+from .views import AbilityScoreList, SkillList, GetAbilityScore, GetSkill
 
 urlpatterns = [
     path("ability_scores/", AbilityScoreList.as_view(), name="ability_scores"),
+    path("ability_scores/<str:name_or_id>/", GetAbilityScore.as_view(), name="ability_scores"),
     path("skills/", SkillList.as_view(), name="skills"),
+    path("skills/<str:name_or_id>/", GetSkill.as_view(), name="skills"),
 ]
--- pydnd/character/views.py
+++ pydnd/character/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import get_object_or_404
 from rest_framework import generics, status
 from rest_framework.response import Response
 from django.forms.models import model_to_dict
+from rest_framework.views import APIView
 
 from .models import AbilityScore, Skill
 from .serializers import AbilityScoreListSerializer, AbilityScoreSerializer, SkillListSerializer, SkillSerializer
@@ -10,13 +11,13 @@ from .serializers import AbilityScoreListSerializer, AbilityScoreSerializer, Ski
 class AbilityScoreList(generics.ListCreateAPIView):
 
     queryset = AbilityScore.objects.all()
-    serializer_class = AbilityScoreListSerializer
+    serializer_class = AbilityScoreSerializer
 
 
 class SkillList(generics.ListCreateAPIView):
 
     queryset = Skill.objects.all()
-    serializer_class = SkillListSerializer
+    serializer_class = SkillSerializer
 
     def post(self, request, *args, **kwargs):
 
@@ -26,11 +27,10 @@ class SkillList(generics.ListCreateAPIView):
         skill = SkillSerializer(data=data)
         if skill.is_valid():
             skill_object = skill.save()
+            skill_object.ability_score = ability_score
+            skill_object = skill.save()
         else:
-            skill_object = get_object_or_404(Skill, name=skill.initial_data['name'])
-
-        skill_object.ability_score = ability_score
-
+            return Response(status=status.HTTP_400_BAD_REQUEST)
         return Response(model_to_dict(skill_object), status.HTTP_200_OK)
 
 
@@ -39,3 +39,30 @@ def get_attribute_by_name(data, attribute_name, model_type):
     attribute_to_get = attribute_value['name']
     attribute_model = get_object_or_404(model_type, name__iexact=attribute_to_get)
     return attribute_model
+
+
+class GetAbilityScore(APIView):
+
+    def get(self, request, name_or_id):
+
+        if name_or_id.isdigit():
+            queryset = get_object_or_404(AbilityScore, id=int(name_or_id))
+        else:
+            queryset = get_object_or_404(AbilityScore, name__iexact=name_or_id)
+        return Response(model_to_dict(queryset), status.HTTP_200_OK)
+
+
+class GetSkill(APIView):
+
+    def get(self, request, name_or_id):
+
+        if name_or_id.isdigit():
+            queryset = get_object_or_404(Skill, id=int(name_or_id))
+        else:
+            queryset = get_object_or_404(Skill, name__iexact=name_or_id)
+
+        skill = model_to_dict(queryset)
+        ability_score = model_to_dict(get_object_or_404(AbilityScore, id=skill['id']))
+        skill['ability_score'] = ability_score
+
+        return Response(skill, status.HTTP_200_OK)
